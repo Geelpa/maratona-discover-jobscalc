7edcab675b845572f92c7f223f85e6a6bcbe5346
Geelpa <geelpa.vayne@gmail.com>
2021-08-20 18:53:40 -0300

    ejs view engine

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const routes = express.Router();
+const views = __dirname + '/views/'
 
-routes.get('/', (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html");
-})
-
-routes.get('/index.html', (req, res) => {
-    return res.redirect('/')
-})
-
-routes.get('/profile.html', (req, res) => {
-    return res.redirect('/')
-})
+routes.get('/', (req, res) => res.render(views + "index"));
+routes.get('/job', (req, res) => res.render(views + "job"));
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
+routes.get('/profile', (req, res) => res.render(views + "profile"));
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const server = express();
 const routes = require('./routes'); 
 
+server.set('view engine', 'ejs')
+
+//habilitar arquivos estaticos
 server.use(express.static('public'))
 
 server.use(routes)
